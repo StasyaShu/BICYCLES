597c87d75914a8ddf74b6c0efa3ac36a3c214c11
StasyaShu <74409974+StasyaShu@users.noreply.github.com>
2022-07-08 18:34:39 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,115 @@
+const loadWindow = (() => {
+  const mainNav = document.querySelector('.main-nav');
+  const promo = document.querySelector('.promo__wrapper');
+  return {
+    removeNojs: () => {
+      mainNav.classList.remove('main-nav--nojs');
+      promo.classList.remove('promo__wrapper--nojs');
+    }
+  }
+})()
+window.addEventListener('load', loadWindow.removeNojs());
+
+const handleMobileMenu = (() => {
+  const siteNavigation = document.querySelector('.main-nav');
+  const siteNavigationToggle = document.querySelector('.main-nav__toggle');
+  const body = document.querySelector('body');
+
+  return {
+    showNav: () => {
+      if (siteNavigationToggle) {
+        siteNavigationToggle.classList.remove('main-nav__toggle--hidden');
+        siteNavigation.classList.add('main-nav--closed');
+
+        siteNavigationToggle.addEventListener('click', (evt) => {
+          evt.preventDefault();
+          siteNavigation.classList.toggle('main-nav--opened');
+          body.style.overflow = 'hidden';
+          siteNavigation.classList.toggle('main-nav--closed');
+
+          if (siteNavigation.classList.contains('main-nav--closed')) {
+            body.style.overflow = 'scroll';
+          }
+        })
+      }
+    },
+    clickNavLink: () => {
+      const siteNavigation = document.querySelector('.main-nav');
+      const siteNavigationList = siteNavigation.querySelector('ul');
+      const siteNavigationItems = siteNavigationList.children;
+
+
+      for (const item of siteNavigationItems) {
+        item.addEventListener('click', () => {
+          siteNavigation.classList.remove('main-nav--opened');
+          siteNavigation.classList.add('main-nav--closed');
+          body.style.overflow = 'scroll';
+        })
+      }
+    }
+  }
+})()
+
+handleMobileMenu.showNav();
+handleMobileMenu.clickNavLink();
+
+
+
+(function scrollToAnchor() {
+  const anchors = document.querySelectorAll('a[href*="#"]');
+  for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault()
+
+      const blockID = anchor.getAttribute('href').substr(1);
+
+      if (document.getElementById(blockID)) {
+        document.getElementById(blockID).scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        })
+      }
+    })
+  }
+})();
+
+(function validateName() {
+  const inputName = document.getElementById('name-id');
+  inputName.addEventListener('invalid', () => {
+    if (inputName.validity.tooShort) {
+      inputName.setCustomValidity('Введите пожалуйста минимум 2 символа');
+    } else if (inputName.validity.tooLong) {
+      inputName.setCustomValidity('Значение не должно превышать 25-ти символов');
+    } else if (inputName.validity.valueMissing) {
+      inputName.setCustomValidity('Введите пожалуйста Ваше имя');
+    } else if (inputName.validity.patternMismatch) {
+      inputName.setCustomValidity('Имя должно состоять из букв русского или английского алфавита');
+    } else {
+      inputName.setCustomValidity('');
+    }
+  })
+})();
+
+(function validateTel() {
+  const inputTel = document.getElementById('tel-id');
+  inputTel.addEventListener('invalid', () => {
+    if (inputTel.validity.valueMissing) {
+      inputTel.setCustomValidity('Введите пожалуйста номер телефона');
+    } else if (inputTel.value.length < 10) {
+      inputTel.setCustomValidity('Номер телефона должен содержать 11 цифр, включая +7');
+    } else {
+      inputTel.setCustomValidity('');
+    }
+  })
+})();
+
+(() => {
+  document.addEventListener('DOMContentLoaded', () => {
+
+    const inputTel = document.getElementById('tel-id');
+    const maskOptions = {
+      mask: '+{7}(000)000-00-00'
+    }
+    IMask(inputTel, maskOptions)
+  })
+})();
